26d63495f7158dee1a207d711580f966691f3c98
galmagrosc <galmagrosc@gmail.com>
2020-11-22 10:44:28 +0100

    5º change in newBranchEclipse1

--- JavaLambdaEclipse/src/LambdaExample.java
+++ JavaLambdaEclipse/src/LambdaExample.java
@@ -8,6 +8,7 @@ public class LambdaExample {
 	* Change from GIT
 	* 2º Change from Eclipse
 	* 3º change from eclipse: newBranchEclipse1
+	* 5º change in newBranchEclipse1
 	*/
 	private List<Map<String, Object>>  getObras() {
 		//Mat1
