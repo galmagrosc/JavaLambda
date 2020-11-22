fec3a8d67bab5d3e8b23e46bfc9dc36a005e9f3c
galmagrosc <galmagrosc@gmail.com>
2020-11-22 12:03:09 +0100

    13º newB

--- JavaLambdaEclipse/src/LambdaExample.java
+++ JavaLambdaEclipse/src/LambdaExample.java
@@ -15,6 +15,7 @@ public class LambdaExample {
 	* 11º change in master
 	* 9º change in newBranchEclipse1
 	* 12º change in newBranchEslipse1
+	* 13º newB.
 	*/
 	private List<Map<String, Object>>  getObras() {
 		//Mat1
